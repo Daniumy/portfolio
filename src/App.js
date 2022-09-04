83cee6d256281d92a71349dc4712bbdd59be0e07
Daniel <danielmaestreyepes@gmail.com>
2022-09-04 14:20:50 +0200

    Add proper metadata + part untranslated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import images from "./utils/images";
 import emailjs from "@emailjs/browser";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import i18next from "i18next";
-import cookies from "js-cookie";
 
 function App() {
   const [airplaneHovered, setAirplaneHovered] = useState(false);
@@ -142,7 +140,7 @@ function App() {
             >
               <rect width="512" height="512" rx="15%" fill={socialsColor} />
               <path
-                fill={socialsColor == "#00203FFF" ? "#fff" : "#00203FFF"}
+                fill={socialsColor === "#00203FFF" ? "#fff" : "#00203FFF"}
                 d="M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-50c-71 16-86-28-86-28-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z"
               />
             </svg>
@@ -198,7 +196,7 @@ function App() {
             : "email-notification"
         }
         style={{
-          backgroundColor: emailStatus == "succesful" ? "lightgreen" : "red",
+          backgroundColor: emailStatus === "succesful" ? "lightgreen" : "red",
         }}
       >
         {emailStatus == "succesful" ? t("email-succesful") : t("email-failed")}
@@ -225,19 +223,19 @@ function App() {
         <div className="navigation-titles-container">
           <h1>
             <a className="nav-titles" href="/#aboutme">
-              About Me
+              {t("aboutme")}
             </a>
           </h1>
           <div className="line-under"></div>
           <h1>
             <a className="nav-titles" href="/#projects">
-              Projects
+            {t("projects")}
             </a>
           </h1>
           <div className="line-under"></div>
           <h1>
             <a className="nav-titles" href="/#contact">
-              Contact
+              {t("contact")}
             </a>
           </h1>
         </div>
