31e455139bb21cac50ab27d4cb501ca3988af34a
Daniel <danielmaestreyepes@gmail.com>
2022-09-04 05:38:34 +0200

    Improved menu hamburguer and socials icons for mobile

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ import cookies from "js-cookie";
 function App() {
   const [airplaneHovered, setAirplaneHovered] = useState(false);
   const [secondScreen, setSecondScreen] = useState(false);
-  const [hamburgerColor, setHamburgerColor] = useState(
-    window.matchMedia("(max-width: 950px)").matches ? "red" : "#00203FFF"
-  );
+  const [hamburgerColor, setHamburgerColor] = useState("#00203FFF");
   const [hamburgerClicked, setHamburgerClicked] = useState(false);
-  const [socialsColor, setSocialsColor] = useState(
-    window.matchMedia("(max-width: 950px)").matches ? "red" : "#00203FFF"
-  );
+  const [socialsColor, setSocialsColor] = useState("#00203FFF");
   const [selectedLanguage, setSelectedLanguage] = useState(
     localStorage.getItem("language") || "es"
   );
   const [emailStatus, setEmailStatus] = useState("none");
+  const [mobileView, setMobileView] = useState(
+    window.matchMedia("(max-width: 950px)").matches
+  );
 
   let navigate = useNavigate();
 
   const [t, i18n] = useTranslation("translation");
-
+    
   function handleResize() {
     if (window.innerWidth < 950) {
-      setHamburgerColor("red");
-      setSocialsColor("red");
+      setMobileView(true);
+    } else {
+      setMobileView(false);
     }
   }
   window.addEventListener("resize", handleResize);
@@ -48,6 +48,7 @@ function App() {
 
   const inputElement = useRef(null);
   const form = useRef();
+
   useEffect(() => {
     inputElement.current.onfocus = () => {
       window.scrollTo(0, 0);
@@ -59,20 +60,16 @@ function App() {
 
   function onScroll() {
     const { scrollTop, scrollHeight } = listInnerRef?.current;
+
     if (scrollTop > 200 && scrollTop < 1100) {
       setSecondScreen(true);
     } else setSecondScreen(false);
 
-    if (listInnerRef.current && hamburgerColor != "red") {
+    if (listInnerRef.current) {
       const pixelesCadaPagina = scrollHeight / NUMERO_PAGINAS;
       const umbralDosPaginas = scrollHeight - pixelesCadaPagina * 2;
-      const under950width = window.matchMedia("(max-width: 950px)").matches;
 
-      if (
-        scrollTop > umbralDosPaginas - 50 ||
-        (scrollTop < pixelesCadaPagina - 50 && under950width)
-      )
-        setHamburgerColor("#ADEFD1FF");
+      if (scrollTop > umbralDosPaginas - 50) setHamburgerColor("#ADEFD1FF");
       else setHamburgerColor("#00203FFF");
       if (scrollTop > pixelesCadaPagina + 50) setSocialsColor("#ADEFD1FF");
       else setSocialsColor("#00203FFF");
@@ -113,11 +110,11 @@ function App() {
 
   return (
     <div className="App" ref={listInnerRef} onScroll={onScroll}>
-      <div
-        className="hamburguer-container"
-        onClick={() => setHamburgerClicked(!hamburgerClicked)}
-      >
-        {!hamburgerClicked && (
+      {!hamburgerClicked && !mobileView && (
+        <div
+          className="hamburguer-container"
+          onClick={() => setHamburgerClicked(!hamburgerClicked)}
+        >
           <svg
             width="45px"
             height="45px"
@@ -131,9 +128,9 @@ function App() {
               fill={hamburgerColor}
             />
           </svg>
-        )}
-      </div>
-      {!hamburgerClicked && (
+        </div>
+      )}
+      {!hamburgerClicked && !mobileView && (
         <div className="social-icons-container">
           <a href="https://github.com/Daniumy" target="_blank" rel="noreferrer">
             <svg
@@ -145,7 +142,7 @@ function App() {
             >
               <rect width="512" height="512" rx="15%" fill={socialsColor} />
               <path
-                fill="white"
+                fill={socialsColor == "#00203FFF" ? "#fff" : "#00203FFF"}
                 d="M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-50c-71 16-86-28-86-28-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z"
               />
             </svg>
@@ -340,6 +337,26 @@ function App() {
         </div>
       </div>
       <div className="first-section">
+        {!hamburgerClicked && mobileView && (
+          <div
+            className="hamburguer-container"
+            onClick={() => setHamburgerClicked(!hamburgerClicked)}
+          >
+            <svg
+              width="45px"
+              height="45px"
+              viewBox="0 0 15 15"
+              fill="#adefd1ff"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z"
+                fill="#adefd1ff"
+              />
+            </svg>
+          </div>
+        )}
         <div className="first-section-row">
           <div className="first-section-left-wrapper">
             <div className="first-section-left-content">
@@ -426,6 +443,71 @@ function App() {
         </div>
       </div>
       <div className="fourth-section" id="contact">
+        {!hamburgerClicked && mobileView && (
+          <div className="social-icons-container">
+            <a
+              href="https://github.com/Daniumy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <svg
+                aria-label="GitHub"
+                role="img"
+                viewBox="0 0 512 512"
+                width="30px"
+                height="30px"
+              >
+                <rect width="512" height="512" rx="15%" fill={"#adefd1ff"} />
+                <path
+                  fill="#00203fff"
+                  d="M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-50c-71 16-86-28-86-28-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z"
+                />
+              </svg>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/daniu"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <svg
+                id="Layer_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 382 382"
+                width="30px"
+                height="30px"
+              >
+                <path
+                  fill={"#adefd1ff"}
+                  d="M347.445,0H34.555C15.471,0,0,15.471,0,34.555v312.889C0,366.529,15.471,382,34.555,382h312.889
+	C366.529,382,382,366.529,382,347.444V34.555C382,15.471,366.529,0,347.445,0z M118.207,329.844c0,5.554-4.502,10.056-10.056,10.056
+	H65.345c-5.554,0-10.056-4.502-10.056-10.056V150.403c0-5.554,4.502-10.056,10.056-10.056h42.806
+	c5.554,0,10.056,4.502,10.056,10.056V329.844z M86.748,123.432c-22.459,0-40.666-18.207-40.666-40.666S64.289,42.1,86.748,42.1
+	s40.666,18.207,40.666,40.666S109.208,123.432,86.748,123.432z M341.91,330.654c0,5.106-4.14,9.246-9.246,9.246H286.73
+	c-5.106,0-9.246-4.14-9.246-9.246v-84.168c0-12.556,3.683-55.021-32.813-55.021c-28.309,0-34.051,29.066-35.204,42.11v97.079
+	c0,5.106-4.139,9.246-9.246,9.246h-44.426c-5.106,0-9.246-4.14-9.246-9.246V149.593c0-5.106,4.14-9.246,9.246-9.246h44.426
+	c5.106,0,9.246,4.14,9.246,9.246v15.655c10.497-15.753,26.097-27.912,59.312-27.912c73.552,0,73.131,68.716,73.131,106.472
+	L341.91,330.654L341.91,330.654z"
+                />
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+              </svg>
+            </a>
+          </div>
+        )}
         <div className="fourth-section-wrapper">
           <h1>{t("contact")}</h1>
           <div
